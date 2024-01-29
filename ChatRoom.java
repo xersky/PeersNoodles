d185ba0923e108d15d1b026347d35e8e6ab3477c
xersky <ayoub.elaoufi@gmail.com>
2024-01-29 04:56:05 +0100

    Testing with threads

--- ChatRoom.java
+++ ChatRoom.java
@@ -1,9 +1,32 @@
-import java.io.IOException;
-import java.net.UnknownHostException;
-
 public class ChatRoom {
-    public static void main(String[] args) throws UnknownHostException, IOException {
+    public static void main(String[] args) throws Exception {
         PeerNode client = new PeerNode();
+/*         Thread threadServer = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    client.startServer(3333);
+                } catch (Exception e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        });  
+        threadServer.start();
+        Thread threadClient = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    client.startConnection("127.0.0.1", 42069);
+                    client.sendMessage("ping");
+                } catch (Exception e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        });  
+        threadClient.start();*/
+
         client.startConnection("127.0.0.1", 42069);
-    }   
+    }    
 }
